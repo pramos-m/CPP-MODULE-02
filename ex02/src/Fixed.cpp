15f7e244f30433bef499657d82742777e40f1e12
Pol Ramos <119877499+pramos-m@users.noreply.github.com>
2023-08-22 19:10:22 +0200

    ex02 done

--- ex02/src/Fixed.cpp
+++ ex02/src/Fixed.cpp
@@ -73,4 +73,153 @@ float	Fixed::toFloat( void ) const
 }
 
 // Operator: Assignation
+Fixed&	Fixed::operator=( const Fixed &fixp )
+{
+	if (this != &fixp)
+		this->_fixp = fixp.getRawBits();
+	return (*this);
+}
 
+// Operator: Assignation: Constant
+Fixed&	Fixed::operator=( Fixed &fixp )
+{
+	if (this != &fixp)
+		this->_fixp = fixp.getRawBits();
+	return (*this);
+}
+
+// Operator: Comparison: '>' == "Greater than"
+bool	Fixed::operator>( const Fixed &fixp ) const
+{
+	return ( this->_fixp > fixp.getRawBits() );
+}
+
+// Operator: Comparison: '<' == "Lower than"
+bool	Fixed::operator<( const Fixed &fixp ) const
+{
+	return ( this->_fixp < fixp.getRawBits() );
+}
+
+// Operator: Comparison: '>=' == "Greater-equal than"
+bool	Fixed::operator>=( const Fixed &fixp ) const
+{
+	return ( this->_fixp >= fixp.getRawBits() );
+}
+
+// Operator: Comparison: '<=' == "Lower-equal than"
+bool	Fixed::operator<=( const Fixed &fixp ) const
+{
+	return ( this->_fixp <= fixp.getRawBits() );
+}
+
+// Operator: Comparison: '==' == "Equal"
+bool	Fixed::operator==( const Fixed &fixp ) const
+{
+	return ( this->_fixp == fixp.getRawBits() );
+}
+
+// Operator: Comparison: '!=' == "Unequal than"
+bool	Fixed::operator!=( const Fixed &fixp ) const
+{
+	return ( this->_fixp != fixp.getRawBits() );
+}
+
+// Operator: Arithmetic: '+' == "Addition"
+Fixed	Fixed::operator+( const Fixed &fixp ) const
+{
+	return ( this->toFloat() + fixp.toFloat() );
+}
+
+// Operator: Arithmetic: '-' == "Subtraction"
+Fixed Fixed::operator-(const Fixed &fixp) const
+{
+    return (this->toFloat() - fixp.toFloat());
+}
+
+// Operator: Arithmetic: '*' == "Mulitiplication"
+Fixed	Fixed::operator*( const Fixed &fixp ) const
+{
+	return ( this->toFloat() * fixp.toFloat() );
+}
+
+// Operator: Arithmetic: '/' == "Division"
+Fixed	Fixed::operator/( const Fixed &fixp ) const
+{
+	return ( this->toFloat() / fixp.toFloat() );
+}
+
+// Operator: Arithmetic: '++' == "Pre-increment"
+Fixed	Fixed::operator++( void )
+{
+	this->_fixp++;
+	return (*this);
+}
+
+// Operator: Arithmetic: '++' == "Post-increment"
+Fixed	Fixed::operator++( int )
+{
+	Fixed	temp(*this);
+	++(*this);
+	return (temp);
+}
+
+// Operator: Arithmetic: '--' == "Pre-decrement"
+Fixed	Fixed::operator--( void )
+{
+	this->_fixp--;
+	return (*this);
+}
+
+// Operator: Arithmetic: '--' == "Post-decrement"
+Fixed	Fixed::operator--( int )
+{
+	Fixed	temp(*this);
+	--(*this);
+	return (temp);
+}
+
+// Static function: Minimun
+Fixed&	Fixed::min( Fixed& first, Fixed& second )
+{
+	if ( first < second )
+		return ( first );
+	return ( second );
+}
+
+// Static function: Constant: Minimun
+const Fixed&	Fixed::min( const Fixed& first, const Fixed& second )
+{
+	if ( first < second )
+		return ( first );
+	return ( second );
+}
+
+// Static function: Maximun
+Fixed&	Fixed::max( Fixed& first, Fixed& second )
+{
+	if ( first > second )
+		return ( first );
+	return ( second );
+}
+
+// Static function: Constant: Maximun
+const Fixed&	Fixed::max( const Fixed& first, const Fixed& second )
+{
+	if ( first > second )
+		return ( first );
+	return ( second );
+}
+
+// Operator: Output
+std::ostream&	operator<<( std::ostream& out, Fixed& fixp )
+{
+	out << fixp.toFloat();
+	return (out);
+}
+
+// Operator: Constant: Output
+std::ostream&	operator<<( std::ostream& out, const Fixed& fixp )
+{
+	out << fixp.toFloat();
+	return (out);
+}
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -15,19 +15,59 @@
 
 int main( void ) 
 {
-	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
-	a = Fixed( 1234.4321f );
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	return 0;
+	Fixed		a;
+	const Fixed	b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed		c( 10.3f );
+	Fixed		d( 10.3f );
+	const Fixed	e( 7.3f );
 
+	std::cout << "a: " << a << std::endl;
+	std::cout << "++a: " << ++a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "a++: " << a++ << std::endl;
+	std::cout << "a: " << a << std::endl;
+
+	std::cout << "b: " << b << std::endl;
+
+	std::cout << "min( a, b ): " << Fixed::min( a, b ) << std::endl;
+
+	std::cout << "max( a, b ): " << Fixed::max( a, b ) << std::endl;
+
+	std::cout << "a > b: " << ( a > b ) << std::endl;
+
+	std::cout << "a >= b: " << ( a >= b ) << std::endl;
+
+	std::cout << "a < b: " << ( a < b ) << std::endl;
+
+	std::cout << "a <= b: " << ( a <= b ) << std::endl;
+
+	std::cout << "min( e, b ): " << Fixed::min( e, b ) << std::endl;
+
+	std::cout << "max( e, b ): " << Fixed::max( e, b ) << std::endl;
+
+	std::cout << "c: " << c << std::endl;
+
+	std::cout << "d: " << d << std::endl;
+
+	std::cout << "c > d: " << ( c > d ) << std::endl;
+
+	std::cout << "c >= d: " << ( c >= d ) << std::endl;
+
+	std::cout << "c < d: " << ( c < d ) << std::endl;
+
+	std::cout << "c <= d: " << ( c <= d ) << std::endl;
+
+	std::cout << "c == d: " << ( c == d ) << std::endl;
+
+	std::cout << "c != d: " << ( c != d ) << std::endl;
+
+	std::cout << "c + d: " << c + d << std::endl;
+
+	std::cout << "c - d: " << c - d << std::endl;
+
+	std::cout << "c * d: " << c * d << std::endl;
+
+	std::cout << "c / d: " << c / d << std::endl;
+
+	return (0);
 }
